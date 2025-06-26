533e793e6a7351b5394d4f0f1f6a92ae712759cb
shy9803 <127914332+shy9803@users.noreply.github.com>
2025-06-26 12:14:17 +0900

    update components

--- components/ItemCard2.js
+++ components/ItemCard2.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import '../style/itemcard2.css';
+import { getTimeAgo } from '../utils/getTimeAgo';
 
-function ItemCard2({ id, imgSrc, brand, name, price, time, link }) {
+function ItemCard2({ id, imgSrc, brand, name, price, datetime }) {
   return (
     <li className="itemcard2">
       <Link to={`/products/${id}`}>
@@ -12,7 +13,7 @@ function ItemCard2({ id, imgSrc, brand, name, price, time, link }) {
         <span className="brand2">{brand}</span>
         <p className="item_name">{name}</p>
         <p className="price">{price}</p>
-        <p className="register_time">{time}</p>
+        <p className="register_time">{getTimeAgo(datetime)}</p>
       </Link>
     </li>
   );
